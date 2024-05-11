80b02f47074da028d547a2a86ef9842d94097d74
Abigail <abby.a.otieno@gmail.com>
2024-05-11 19:04:45 +0100

    Added GET request for rates

--- server/app.py
+++ server/app.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask import Flask,jsonify
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/home',methods = ['GET'])
+def home():
+    request = requests.get(url)
+    json_data = json.loads(request.content)
+    print(json_data)
+
+
+
+if __name__=='__main__':
+    url = 'https://api.exchangerate-api.com/v4/latest/USD'
+    home()
